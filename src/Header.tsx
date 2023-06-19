8853a3ce872bf809f7ce288839437d283551b6b0
hieptth <hieptth71@gmail.com>
2023-06-19 22:42:31 +0700

    comment Header for production

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,53 +1,53 @@
-import { useState } from "react";
-import Axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-uuidv4();
+// import { useState } from "react";
+// import Axios from "axios";
+// import { v4 as uuidv4 } from "uuid";
+// uuidv4();
 
-const Header = () => {
-  const users = [
-    { id: uuidv4(), name: "Hak", age: 22, email: "hieptth71@gmail.com" },
-    { id: uuidv4(), name: "Jake", age: 15, email: "jakieluv22@gmail.com" },
-    { id: uuidv4(), name: "Pedro", age: 27, email: "pedrotech@gmail.com" },
-  ];
+// const Header = () => {
+//   const users = [
+//     { id: uuidv4(), name: "Hak", age: 22, email: "hieptth71@gmail.com" },
+//     { id: uuidv4(), name: "Jake", age: 15, email: "jakieluv22@gmail.com" },
+//     { id: uuidv4(), name: "Pedro", age: 27, email: "pedrotech@gmail.com" },
+//   ];
 
-  const [inputName, setInputName] = useState("");
-  const [predictedAge, setPredictedAge] = useState(null);
+//   const [inputName, setInputName] = useState("");
+//   const [predictedAge, setPredictedAge] = useState(null);
 
-  const fetchData = () => {
-    Axios.get(`https://api.agify.io/?name=${inputName}`).then((res) =>
-      setPredictedAge(res.data.age)
-    );
-  };
+//   const fetchData = () => {
+//     Axios.get(`https://api.agify.io/?name=${inputName}`).then((res) =>
+//       setPredictedAge(res.data.age)
+//     );
+//   };
 
-  return (
-    <>
-      <h2>List of User</h2>
-      <input
-        type="text"
-        name="inputName"
-        placeholder="Type in the name ..."
-        onChange={(e) => {
-          setInputName(e.target.value);
-        }}
-      />
-      <button onClick={fetchData}>Press me!</button>
-      <p>Predicted age: {predictedAge}</p>
-      <div className="userList">
-        {users.map((value) => (
-          <div key={value.id}>
-            <hr />
-            <User
-              key={value.id}
-              name={value.name}
-              age={value.age}
-              email={value.email}
-            />
-          </div>
-        ))}
-        ;
-      </div>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <h2>List of User</h2>
+//       <input
+//         type="text"
+//         name="inputName"
+//         placeholder="Type in the name ..."
+//         onChange={(e) => {
+//           setInputName(e.target.value);
+//         }}
+//       />
+//       <button onClick={fetchData}>Press me!</button>
+//       <p>Predicted age: {predictedAge}</p>
+//       <div className="userList">
+//         {users.map((value) => (
+//           <div key={value.id}>
+//             <hr />
+//             <User
+//               key={value.id}
+//               name={value.name}
+//               age={value.age}
+//               email={value.email}
+//             />
+//           </div>
+//         ))}
+//         ;
+//       </div>
+//     </>
+//   );
+// };
 
-export default Header;
+// export default Header;
